8da4d4df3719d28fbf399eb61fd0e65fbeb4925d
TheSnakeByte <127603693+TheSnakeByte@users.noreply.github.com>
2023-05-16 18:53:19 +0100

    Delete updated directory

--- updated/script.js
+++
@@ -1,9 +0,0 @@
-// This script is linked to [root]updated/updated.html
-
-// Inserts version after fetching it from manifest.json
-fetch('/manifest.json')
-  .then(response => response.text())
-  .then((data) => {
-    var content = JSON.parse(data)
-    document.getElementById("version").innerHTML = content.version;
-  })
